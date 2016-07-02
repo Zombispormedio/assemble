e097fb5a531dc84492c2ef3099af9a7a659b7b48
Xavier Serrano <zombispormedio007@gmail.com>
2016-07-02 21:07:02 +0200

    navigation drawer panel

--- app/src/main/java/com/zombispormedio/assemble/HomeActivity.java
+++ app/src/main/java/com/zombispormedio/assemble/HomeActivity.java
@@ -1,33 +1,48 @@
 package com.zombispormedio.assemble;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private  FirebaseAuth.AuthStateListener mAuthListener;
+    private Toolbar homebar;
+    private DrawerLayout drawer;
+    private NavigationView nav;
+    private HomeDrawerTools drawerTools;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
         mAuth=FirebaseAuth.getInstance();
 
         mAuthListener=FirebaseTools.checkAccess(this, Navigation.Type.HOME);
+        drawer= (DrawerLayout)findViewById(R.id.drawer_layout_home);
+        nav=(NavigationView)findViewById(R.id.navview);
+        drawerTools=new HomeDrawerTools(this, drawer,nav);
 
-        FloatingActionButton signoutFab=(FloatingActionButton) findViewById(R.id.signout_button);
-        signoutFab.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                Navigation.Login(HomeActivity.this);
-            }
-        });
+        homebar=(Toolbar) findViewById(R.id.home_bar);
+        setSupportActionBar(homebar);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.account_circle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
     }
 
     @Override
@@ -48,4 +63,17 @@ public class HomeActivity extends AppCompatActivity {
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
--- app/src/main/java/com/zombispormedio/assemble/HomeDrawerTools.java
+++ app/src/main/java/com/zombispormedio/assemble/HomeDrawerTools.java
@@ -1,7 +1,74 @@
 package com.zombispormedio.assemble;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * Created by Master on 02/07/2016.
  */
 public class HomeDrawerTools {
+    private Context ctx;
+    private Activity view;
+    private DrawerLayout drawer;
+    private NavigationView nav;
+    private FirebaseAuth mAuth;
+
+    public HomeDrawerTools(Context ctx,DrawerLayout drawer,NavigationView nav ){
+        this.ctx=ctx;
+        view=(Activity)ctx;
+        this.drawer=drawer;
+        this.nav=nav;
+        mAuth= FirebaseAuth.getInstance();
+        init();
+
+    }
+
+    private void init(){
+        nav.setNavigationItemSelectedListener(navListener());
+        drawer.addDrawerListener(DrawerListener());
+
+
+    }
+
+    private NavigationView.OnNavigationItemSelectedListener navListener(){
+        return new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch(item.getItemId()){
+                    case R.id.signout_menu:
+                        mAuth.signOut();
+                        Navigation.Login(ctx);
+                        break;
+                }
+                drawer.closeDrawers();
+                return true;
+            }
+        };
+    }
+
+    private ActionBarDrawerToggle DrawerListener(){
+        return new ActionBarDrawerToggle(view, drawer, R.string.open_drawer_desc, R.string.close_drawer_desc){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+                TextView nav_title=(TextView)view.findViewById(R.id.nav_title);
+
+                nav_title.setText(mAuth.getCurrentUser().getEmail());
+
+            }
+        };
+    }
+
+
 }
