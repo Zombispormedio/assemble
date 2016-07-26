dc45372706d34c10874520e3cd5a914b5bc3eef9
Xavier Serrano <zombispormedio007@gmail.com>
2016-07-26 15:16:35 +0200

    defaultResponse

--- app/src/main/java/com/zombispormedio/assemble/rest/AbstractResponse.java
+++ app/src/main/java/com/zombispormedio/assemble/rest/AbstractResponse.java
@@ -7,17 +7,20 @@ import org.json.JSONObject;
 /**
  * Created by Master on 26/07/2016.
  */
-public class Response {
+public abstract class AbstractResponse<T> {
 
     public final boolean success;
 
     public final Error error;
 
-    public final Result result;
+    public final T result;
 
-    public Response(boolean success, Error error, Result result) {
+    public AbstractResponse(boolean success, Error error, T result) {
         this.success = success;
         this.error = error;
         this.result = result;
     }
+
+
+
 }
