34a46f45b047999fc823c6ed832672a1a7439c6f
Xavier Serrano <zombispormedio007@gmail.com>
2016-08-01 15:40:44 +0200

    get images

---
+++ app/src/main/java/com/zombispormedio/assemble/rest/FileBody.java
@@ -0,0 +1,7 @@
+package com.zombispormedio.assemble.rest;
+
+/**
+ * Created by Xavier Serrano on 01/08/2016.
+ */
+public class FileBody {
+}
---
+++ app/src/main/java/com/zombispormedio/assemble/utils/ExternalNavigationManager.java
@@ -0,0 +1,7 @@
+package com.zombispormedio.assemble.utils;
+
+/**
+ * Created by Xavier Serrano on 01/08/2016.
+ */
+public class ExternalNavigationManager {
+}
