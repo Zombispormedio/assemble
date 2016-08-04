fbe9ef6b309b3dabde01ce6ad5bf31c70de43065
Xavier Serrano <zombispormedio007@gmail.com>
2016-08-04 12:47:23 +0200

    Update Profile

---
+++ app/src/main/java/com/zombispormedio/assemble/controllers/UpdateProfileController.java
@@ -0,0 +1,8 @@
+package com.zombispormedio.assemble.controllers;
+
+/**
+ * Created by Xavier Serrano on 04/08/2016.
+ */
+public class UpdateProfileController {
+
+}
---
+++ app/src/main/java/com/zombispormedio/assemble/views/IUpdateProfileView.java
@@ -0,0 +1,8 @@
+package com.zombispormedio.assemble.views;
+
+/**
+ * Created by Xavier Serrano on 04/08/2016.
+ */
+public interface IUpdateProfileView {
+
+}
