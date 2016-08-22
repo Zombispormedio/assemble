21df72282b4978ae8b25068fbc26dbfb6649de34
Xavier Serrano <zombispormedio007@gmail.com>
2016-08-22 14:58:24 +0200

    update profile

---
+++ app/src/main/java/com/zombispormedio/assemble/activities/UpdateBirthdateActivity.java
@@ -0,0 +1,15 @@
+package com.zombispormedio.assemble.activities;
+
+import com.zombispormedio.assemble.R;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateBirthdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_birthdate);
+    }
+}
