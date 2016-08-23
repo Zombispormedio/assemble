a4223e2b74e55f72174ef3625f33c16515451768
Xavier Serrano <zombispormedio007@gmail.com>
2016-08-23 13:52:41 +0200

    updatebirthdate datepicker

---
+++ app/src/main/java/com/zombispormedio/assemble/controllers/UpdateBirthdateController.java
@@ -0,0 +1,8 @@
+package com.zombispormedio.assemble.controllers;
+
+/**
+ * Created by Xavier Serrano on 23/08/2016.
+ */
+public class UpdateBirthdateController {
+
+}
---
+++ app/src/main/java/com/zombispormedio/assemble/utils/DateUtils.java
@@ -0,0 +1,8 @@
+package com.zombispormedio.assemble.utils;
+
+/**
+ * Created by Xavier Serrano on 23/08/2016.
+ */
+public class DateUtils {
+
+}
